18d21df1b3632820479881c299a361213b63dacd
Jae <me@jaehan.com>
2019-02-16 13:02:42 -0500

    use correct kue UI package

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 let kue = require('kue');
 let queue = kue.createQueue();
 let express = require('express');
+let ui = require('kue-ui-express');
+let app = express();
+
+// Configure & mount Kue UI
+ui(app, '/kue/', '/api/')
+
+// Mount Kue JSON API
+app.use('/api', kue.app);
+
+// Listen on port 5000
+app.listen(5000);
+
+console.log('Listening on port 5000');
